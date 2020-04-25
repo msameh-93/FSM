229c94015cf48d11e0d472101d43f51198f1960a
msameh-93 <msame.93@hotmail.com>
2020-04-26 00:29:17 +0200

    Heroku deployment + MYSQL

---
+++ src/main/resources/static/precache-manifest.5f9830e274ca082e4f9682d30ccb3f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a9bd6c66d2eda4c9d2605b1ba76e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e17ce5694ad2a7cfcc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "184e8386c4df8dcedc20",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "a9e17ce5694ad2a7cfcc",
+    "url": "/static/js/2.972badc6.chunk.js"
+  },
+  {
+    "revision": "773b818ba24ebcc45b72e93d5ca3cfad",
+    "url": "/static/js/2.972badc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184e8386c4df8dcedc20",
+    "url": "/static/js/main.4cc2ea09.chunk.js"
+  },
+  {
+    "revision": "6329c85a5397e875a6a6",
+    "url": "/static/js/runtime-main.57a6e6f9.js"
+  }
+]);
